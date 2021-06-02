6ee856e35db2be156909433bb9cb922c38bbb466
Stephen Okpalaononuju <stephenokpala@gmail.com>
2021-06-02 06:18:36 +0100

    ft-build-github-bot

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,102 @@
-import { Probot } from "probot";
+import { Probot } from "probot"
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  const openPR = "pull_request.opened"
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-};
+  app.on(openPR, async context => {
+    try {
+      const pullRequest = context.payload.pull_request
+
+      let labels: any
+
+      pullRequest.title.includes("Haystack")
+        ? (labels = ["Haystack"])
+        : (labels = null)
+
+      const addLabelToPR = context.issue({ labels })
+
+      return await context.octokit.issues.addLabels(addLabelToPR)
+    } catch (error) {
+      throw error
+    }
+  })
+
+  app.on(openPR, async context => {
+    try {
+      const owner = context.payload.repository.owner.login
+      const repo = context.payload.repository.name
+
+      const listPR = await context.octokit.rest.pulls.list({ owner, repo })
+
+      const size = listPR.data.map(list => {
+        return list.number
+      })
+
+      const repoSize = context.payload.repository.size
+      const additions = context.payload.pull_request.additions
+      const deletions = context.payload.pull_request.deletions
+      const PRSize = additions + deletions
+      const allPRs = size[0]
+      const averagePRSize = repoSize / allPRs
+
+      const percentDifference = (averagePRSize: number, PRSize: number) => {
+        const percentage =
+          100 *
+          Math.abs((averagePRSize - PRSize) / ((averagePRSize + PRSize) / 2))
+
+        return Math.round(percentage)
+      }
+
+      const body =
+        PRSize > averagePRSize
+          ? `This pr has ${PRSize} size. It's is ${percentDifference(
+              averagePRSize,
+              PRSize
+            )}% more than the average pr made to this pr.`
+          : `This pr has ${PRSize} size. It's is ${percentDifference(
+              averagePRSize,
+              PRSize
+            )}% less than the average pr made to this pr.`
+
+      const addComment = context.issue({ body })
+
+      return await context.octokit.issues.createComment(addComment)
+    } catch (error) {
+      throw error
+    }
+  })
+
+  app.on("pull_request.closed", async context => {
+    try {
+      if (context.payload.pull_request.merged) {
+        const closedAt: any = context.payload.pull_request.closed_at
+        const createdAt = context.payload.pull_request.created_at
+
+        const timeDifference = (createdAt: string, closedAt: string) => {
+          const a = new Date(createdAt).getTime()
+          const b = new Date(closedAt).getTime()
+
+          const milliseconds = b - a
+          const minutes = Math.round(milliseconds / 60 / 1000)
+          const hours = Math.round(milliseconds / 3600 / 1000)
+          const newMinutes = minutes - 60 * hours
+
+          return hours > 0
+            ? `${hours} hours ${newMinutes} minutes`
+            : `${newMinutes} minutes`
+        }
+
+        const addTimeDifference = context.issue({
+          body: `This pr took ${timeDifference(
+            createdAt,
+            closedAt
+          )} to complete`,
+        })
+
+        return await context.octokit.issues.createComment(addTimeDifference)
+      }
+    } catch (error) {
+      throw error
+    }
+  })
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,26 +1,26 @@
 // You can import your modules
 // import index from '../src/index'
 
-import nock from "nock";
+import nock from "nock"
 // Requiring our app implementation
-import myProbotApp from "../src";
-import { Probot, ProbotOctokit } from "probot";
+import myProbotApp from "../src"
+import { Probot, ProbotOctokit } from "probot"
 // Requiring our fixtures
-import payload from "./fixtures/issues.opened.json";
-const issueCreatedBody = { body: "Thanks for opening this issue!" };
-const fs = require("fs");
-const path = require("path");
+import payload from "./fixtures/issues.opened.json"
+const issueCreatedBody = { body: "Thanks for opening this issue!" }
+const fs = require("fs")
+const path = require("path")
 
 const privateKey = fs.readFileSync(
   path.join(__dirname, "fixtures/mock-cert.pem"),
   "utf-8"
-);
+)
 
 describe("My Probot app", () => {
-  let probot: any;
+  let probot: any
 
   beforeEach(() => {
-    nock.disableNetConnect();
+    nock.disableNetConnect()
     probot = new Probot({
       appId: 123,
       privateKey,
@@ -29,12 +29,12 @@ describe("My Probot app", () => {
         retry: { enabled: false },
         throttle: { enabled: false },
       }),
-    });
+    })
     // Load our app into probot
-    probot.load(myProbotApp);
-  });
+    probot.load(myProbotApp)
+  })
 
-  test("creates a comment when an issue is opened", async (done) => {
+  test("creates a comment when an issue is opened", async done => {
     const mock = nock("https://api.github.com")
       // Test that we correctly return a test token
       .post("/app/installations/2/access_tokens")
@@ -47,22 +47,22 @@ describe("My Probot app", () => {
 
       // Test that a comment is posted
       .post("/repos/hiimbex/testing-things/issues/1/comments", (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody));
-        return true;
+        done(expect(body).toMatchObject(issueCreatedBody))
+        return true
       })
-      .reply(200);
+      .reply(200)
 
     // Receive a webhook event
-    await probot.receive({ name: "issues", payload });
+    await probot.receive({ name: "issues", payload })
 
-    expect(mock.pendingMocks()).toStrictEqual([]);
-  });
+    expect(mock.pendingMocks()).toStrictEqual([])
+  })
 
   afterEach(() => {
-    nock.cleanAll();
-    nock.enableNetConnect();
-  });
-});
+    nock.cleanAll()
+    nock.enableNetConnect()
+  })
+})
 
 // For more information about testing with Jest see:
 // https://facebook.github.io/jest/
